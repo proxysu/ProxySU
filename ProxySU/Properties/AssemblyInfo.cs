6f1eacfa302c0c11f6251785aeddd193ee7b5e5e
ProxySU <proxysetuptools@gmail.com>
2020-05-06 12:15:09 +0800

    生成ReleaseBete

--- ProxySU/Properties/AssemblyInfo.cs
+++ ProxySU/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ProxySU")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("V2ray，Trojan，NaiveProxy一键安装工具")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ProxySU")]
 [assembly: AssemblyProduct("ProxySU")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
