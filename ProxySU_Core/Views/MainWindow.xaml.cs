f27ae4d70cf91da2d561412c3ca375672a01660b
nuxt-autumn <lhf939@gmail.com>
2021-03-28 09:11:10 +0800

    v3.1.0

--- ProxySU_Core/Views/MainWindow.xaml.cs
+++ ProxySU_Core/Views/MainWindow.xaml.cs
@@ -5,24 +5,14 @@ using ProxySU_Core.Common;
 using ProxySU_Core.Models;
 using ProxySU_Core.ViewModels;
 using ProxySU_Core.Views;
-using Renci.SshNet;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 
 namespace ProxySU_Core
