22e060e14aa9bfcac93e0502a288b8796995645f
next-autumn <next-autumn@google.com>
2021-05-08 18:52:59 +0800

    mvvmcross

---
+++ ProxySuper.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxySuper.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+
+        public MainViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        private int _count = 1;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                SetProperty(ref _count, value);
+            }
+        }
+
+        public IMvxCommand PlusCommand => new MvxCommand(Plus);
+
+        public void Plus()
+        {
+            this.Count++;
+
+            if (this.Count >= 2)
+            {
+                _navigationService.Navigate<SecondViewModel>();
+            }
+        }
+    }
+}
---
+++ ProxySuper.Core/ViewModels/SecondViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxySuper.Core.ViewModels
+{
+    public class SecondViewModel : MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+
+        public SecondViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public string Message { get; set; } = "Hello world!";
+
+        public IMvxCommand BackCommand => new MvxCommand(Back);
+
+        public void Back()
+        {
+            _navigationService.Close(this);
+        }
+    }
+}
