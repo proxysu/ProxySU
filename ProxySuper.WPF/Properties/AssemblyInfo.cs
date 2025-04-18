ff818ef854e2560725a9a10b8b19584bb4c2bb71
MisCusi2023 <mygithub323@gmail.com>
2025-04-17 20:10:41 -0700

    test

--- ProxySuper.WPF/Properties/AssemblyInfo.cs
+++ ProxySuper.WPF/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("25.3.21.0")]
-[assembly: AssemblyFileVersion("25.3.21.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
